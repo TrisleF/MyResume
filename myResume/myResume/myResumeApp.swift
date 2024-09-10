e0bf57f887cdb4863f4e23021a951070844e32a3
TrisleF <124776876+TrisleF@users.noreply.github.com>
2024-09-10 14:56:59 +0900

    Add files via upload

---
+++ myResume/myResume/myResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  myResumeApp.swift
+//  myResume
+//
+//  Created by Trisle on 2024/08/20.
+//
+
+import SwiftUI
+
+@main
+struct myResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
